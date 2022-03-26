d7de0394529d4d7faa56d260d683841642d335d2
Victor Brandão <victor.brandao596@gmail.com>
2022-03-26 09:34:19 -0300

    Update base-de-produtos.js

--- script/base-de-produtos.js
+++ script/base-de-produtos.js
@@ -1,6 +1,6 @@
 const posts = [{
         id: '0',
-        imagem: "/imgs and vectors/x570.jpg",
+        imagem: "./imgs and vectors/x570.jpg",
         categoria: 'Placa-mãe',
         nome: 'Placa-Mãe Asus ROG Strix X570',
         descricao: 'A placa-mãe ROG Strix X570 oferece a experiência ROG...',
@@ -9,7 +9,7 @@ const posts = [{
     },
     {
         id: '1',
-        imagem: "/imgs and vectors/b660.jpg",
+        imagem: "./imgs and vectors/b660.jpg",
         categoria: 'Placa-mãe',
         nome: 'Placa-mãe Asus ROG Strix B660',
         descricao: 'A ROG Strix B660-F Gaming WiFi oferece fornecimento de energia premium e...',
@@ -18,7 +18,7 @@ const posts = [{
     },
     {
         id: '2',
-        imagem: "/imgs and vectors/b550.jpg",
+        imagem: "./imgs and vectors/b550.jpg",
         categoria: 'Placa-mãe',
         nome: 'Placa-Mãe Asus ROG Strix B550',
         descricao: 'As placas-mãe da série ROG Strix B550 Gaming oferecem um conjunto de recursos...',
@@ -27,7 +27,7 @@ const posts = [{
     },
     {
         id: '3',
-        imagem: "/imgs and vectors/b660white.jpg",
+        imagem: "./imgs and vectors/b660white.jpg",
         categoria: 'Placa-mãe',
         nome: 'Placa-mãe Asus ROG B660 White',
         descricao: 'A ROG Strix B660-A Gaming WiFi D4 oferece fornecimento de energia premium...',
@@ -36,7 +36,7 @@ const posts = [{
     },
     {
         id: '4',
-        imagem: "/imgs and vectors/MONITORROG.jpg",
+        imagem: "./imgs and vectors/MONITORROG.jpg",
         categoria: 'Monitor',
         nome: 'Monitor Gamer Asus ROG Strix',
         descricao: '165 Hz, 2K QHD, 1ms, FreeSync Premium, HDR 400, 125% sRGB, HDMI/DisplayPort',
@@ -45,7 +45,7 @@ const posts = [{
     },
     {
         id: '5',
-        imagem: "/imgs and vectors/MONITORWIDE.jpg",
+        imagem: "./imgs and vectors/MONITORWIDE.jpg",
         categoria: 'Monitor',
         nome: 'Monitor Gamer Asus ROG Swift',
         descricao: 'Ultra Wide, 144 Hz, DFHD, FreeSync Premium, HDR 10, 100% sRGB, HDMI/DisplayPort',
@@ -54,7 +54,7 @@ const posts = [{
     },
     {
         id: '6',
-        imagem: "/imgs and vectors/rogportatil.jpg",
+        imagem: "./imgs and vectors/rogportatil.jpg",
         categoria: 'Monitor',
         nome: 'Monitor Gamer Portátil ASUS',
         descricao: 'Full HD, IPS, Micro HDMI/DisplayPort, 240Hz, 3ms',
@@ -64,7 +64,7 @@ const posts = [{
     },
     {
         id: '7',
-        imagem: "/imgs and vectors/rogUltraWide.jpg",
+        imagem: "./imgs and vectors/rogUltraWide.jpg",
         categoria: 'Monitor',
         nome: 'Monitor Asus ROG Strix ',
         descricao: 'Ultra Wide, 100 Hz, 2K QHD, FreeSync, 100% sRGB, HDMI/DisplayPort, Ajuste de Altura',
@@ -74,7 +74,7 @@ const posts = [{
     },
     {
         id: '8',
-        imagem: "/imgs and vectors/placadevideoROGWHITE.jpg",
+        imagem: "./imgs and vectors/placadevideoROGWHITE.jpg",
         categoria: 'Placa de vídeo',
         nome: 'Asus Rog Strix GeForce RTX 3090',
         descricao: '24GB, GDDR6 - ROG-STRIX-RTX3090-O24G-WHITE',
@@ -83,7 +83,7 @@ const posts = [{
     },
     {
         id: '9',
-        imagem: "/imgs and vectors/placadevideoRGB.jpg",
+        imagem: "./imgs and vectors/placadevideoRGB.jpg",
         categoria: 'Placa de vídeo',
         nome: 'ROG STRIX LC RX6900XT T16G',
         descricao: '16 Gbps, 16GB GDDR6, RGB, RDNA 2 Architecture',
@@ -92,7 +92,7 @@ const posts = [{
     },
     {
         id: '10',
-        imagem: "/imgs and vectors/placadevideoSTRIX.jpg",
+        imagem: "./imgs and vectors/placadevideoSTRIX.jpg",
         categoria: 'Placa de vídeo',
         nome: 'Asus NVIDIA GeForce RTX 3050',
         descricao: 'NVIDIA GeForce RTX 3050 ROG Strix Gaming OC, 8GB, GDDR6',
