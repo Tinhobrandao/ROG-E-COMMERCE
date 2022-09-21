aee1a0de170b0597d0bdba89778af9dc3583a37f
Vit0rg <vgois032@gmail.com>
2022-09-21 19:05:52 -0300

    Refatoração completa do projeto, falta responsivo.

--- script/filterList-&-search.js
+++ script/filterList-&-search.js
@@ -1,33 +1,37 @@
-document.querySelector('.itensMenu').addEventListener('click', e => {
-    const filtrarID = e.target.dataset.tag
-    const newProducts = []
+document.querySelector('.itensMenu').addEventListener('click', e => 
+{
+  const filtrarID = e.target.dataset.tag
+  const newProducts = []
 
-    for (let i = 0; i < posts.length; i++) {
-        if (posts[i].categoria.includes(filtrarID)) {
-            console.log(posts[i].categoria)
-            document.querySelector('.listaDeProdutos').remove('.listaDeProdutos')
-            newProducts.push(posts[i])
-            criarCard(newProducts)
-        } else if (filtrarID === 'Todos') {
-            document.querySelector('.listaDeProdutos').remove('.listaDeProdutos')
-            criarCard(posts)
-        }
+  for (let i = 0; i < posts.length; i++) 
+  {
+    if (posts[i].categoria.includes(filtrarID)) 
+    {
+      document.querySelector('.listaDeProdutos').remove('.listaDeProdutos')
+      newProducts.push(posts[i])
+      criarCard(newProducts)
+    } else if (filtrarID === 'Todos') 
+    {
+      document.querySelector('.listaDeProdutos').remove('.listaDeProdutos')
+      criarCard(posts)
     }
+  }
 })
 
-const inputDePesquisa = document.querySelector('button')
-inputDePesquisa.addEventListener('click', filtraItens)
+const inputDePesquisa = document.querySelector('#pesquisa')
+inputDePesquisa.addEventListener('input', filtraItens)
 
-function filtraItens() {
-    const input = document.querySelector('input')
-    const textoDigitado = input.value
-    const newArray = []
-    for (let i = 0; i < posts.length; i++) {
-        if (posts[i].categoria.replaceAll('-', ' ').replaceAll(' ', '').toLowerCase().includes(textoDigitado.replaceAll('-', ' ').replaceAll(' ', '').toLowerCase())) {
-            document.querySelector('.listaDeProdutos').remove('.listaDeProdutos')
-            newArray.push(posts[i])
-            criarCard(newArray)
-        }
+function filtraItens() 
+{
+  const textoDigitado = inputDePesquisa.value
+  const newArray = []
+  for (let i = 0; i < posts.length; i++) 
+  {
+    if (posts[i].categoria.replaceAll('-', ' ').replaceAll(' ', '').toLowerCase().includes(textoDigitado.replaceAll('-', ' ').replaceAll(' ', '').toLowerCase())) 
+    {
+      document.querySelector('.listaDeProdutos').remove('.listaDeProdutos')
+      newArray.push(posts[i])
+      criarCard(newArray)
     }
-
+  }
 }
